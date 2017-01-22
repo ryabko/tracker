81e10ae01c5dc34eb01d205b2f7e91a5e6876255
Sergey Shestopalov <ryabko.sergey@gmail.com>
2017-01-23 00:47:23 +0300

    Разбиение кода игры на небольшие модули. Окошко для ввода пина и обработка событий в нем (без реализации взаимодействия с сервером).

--- front/app/scripts/game.js
+++ front/app/scripts/game.js
@@ -1,21 +1,111 @@
-function initGame(ymaps) {
-    var map = new ymaps.Map('map', appSettings.mapOptions);
+var map = (function() {
+    var _map = null;
+    var _me = null;
 
-    (function () {
-        var me = null;
-        if (navigator.geolocation) {
-            navigator.geolocation.watchPosition(function (position) {
-                var coords = [position.coords.latitude, position.coords.longitude];
-
-                if (me == null) {
-                    me = new ymaps.Placemark(coords, {}, {preset: 'islands#redCircleDotIcon'});
-                    map.geoObjects.add(me);
-                    map.setCenter(coords);
-                } else {
-                    me.geometry.setCoordinates(coords);
-                }
-            }, function () {}, appSettings.posWatchOptions);
+    return {
+        init: function(ymaps, mapId) {
+            _map = new ymaps.Map(mapId, appSettings.mapOptions);
+        },
+        showMe: function(coords) {
+            if (_me == null) {
+                _me = new ymaps.Placemark(coords, {}, {preset: 'islands#redCircleDotIcon'});
+                _map.geoObjects.add(_me);
+                _map.setCenter(coords);
+            } else {
+                _me.geometry.setCoordinates(coords);
+            }
         }
-    })();
+    }
+}());
 
+var game = (function() {
+    var _myPos = null;
+    var _onUpdate = null;
+
+    return {
+        init: function(ymaps, mapId, onInit) {
+            map.init(ymaps, mapId);
+            navigator.geolocation.watchPosition(function (position) {
+                var _myPos = [position.coords.latitude, position.coords.longitude];
+                console.log("my position: " + _myPos);
+                map.showMe(_myPos);
+            }, function () {}, appSettings.posWatchOptions);
+
+            onInit();
+        },
+        connect: function(callbacks) {
+            _onUpdate = callbacks.onUpdate;
+            console.log("Game connecting....");
+            callbacks.onConnect();
+        },
+        disconnect: function(onDisconnect) {
+            console.log("Game disconnecting....");
+            onDisconnect();
+        }
+    }
+}());
+
+var dashboard = (function() {
+    var connect = function(pin) {
+        showBlock("loading");
+        game.connect({
+            onConnect: function() {
+                $pinSpan.text(pin);
+                showBlock("info");
+            },
+            onUpdate: function() {
+                console.log("Update callback");
+            }
+        });
+    };
+
+    var disconnect = function() {
+        showBlock("loading");
+        game.disconnect(function() {
+            $pinSpan.text("");
+            $pinEdit.val("");
+            showBlock("input");
+        });
+    };
+
+    var showBlock = function(block) {
+        $pinInputBlock.toggle(block == "input");
+        $loadingBlock.toggle(block == "loading");
+        $infoBlock.toggle(block == "info");
+    };
+
+    var $dashboard = $("#dashboard");
+    var $pinInputBlock = $("#pin-input-block");
+    var $loadingBlock = $("#loading-block");
+    var $infoBlock = $("#info-block");
+    var $pinEdit = $("#pin-edit");
+    var $startBtn = $("#start-btn");
+    var $exitBtn = $("#exit-btn");
+    var $pinSpan = $("#pin-span");
+
+    $startBtn.on("click", function() {
+        connect($pinEdit.val());
+    });
+
+    $pinEdit.keyup(function(e) {
+        if (e.keyCode == 13) {
+            $startBtn.click();
+        }
+    });
+
+    $exitBtn.on("click", function() {
+        disconnect();
+    });
+
+    return {
+        show: function() {
+            $dashboard.show();
+        }
+    }
+}());
+
+function initGame(ymaps) {
+    game.init(ymaps, "map", function() {
+        dashboard.show();
+    });
 }
